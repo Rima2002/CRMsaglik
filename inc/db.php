b84f57e744e04d26d3aa6529a9ffc47b6c0e5294
Rima Farah Eleuch <66773572+Rima2002@users.noreply.github.com>
2025-06-15 15:57:28 +0300

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -1,13 +1,13 @@
 <?php
 // Veritabanı bağlantı bilgileri
 $host = 'localhost';            
-$dbname = 'dbstorage21360859216';         
+$dbname = 'crm_saglik';         
 $username = 'root';            
 $password = '';                
 
 try {
     // PDO nesnesi ile veritabanına bağlanılıyor
-    $pdo = new PDO("mysql:host=localhost;dbname=dbstorage21360859216", "dbusr21360859216", "9v64V7k2JLPp");
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 
     // PDO'nun hata ayıklama modunu açıyoruz (hatalar Exception olarak fırlatılır)
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
